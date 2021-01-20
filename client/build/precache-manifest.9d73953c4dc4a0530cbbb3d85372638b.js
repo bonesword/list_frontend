61de406ba2d643de4967b33f550f198fcfcdec25
sabin <sabinshrestha292@gmail.com>
2021-01-20 20:38:53 +0545

    server created to server client file

---
+++ client/build/precache-manifest.9d73953c4dc4a0530cbbb3d85372638b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8995917a490cad3749ba",
+    "url": "/static/js/main.cf308744.chunk.js"
+  },
+  {
+    "revision": "e8ea4f0c99865e83adc3",
+    "url": "/static/js/2.eafb0d20.chunk.js"
+  },
+  {
+    "revision": "8995917a490cad3749ba",
+    "url": "/static/css/main.ecba992a.chunk.css"
+  },
+  {
+    "revision": "e8ea4f0c99865e83adc3",
+    "url": "/static/css/2.91d6e3ee.chunk.css"
+  },
+  {
+    "revision": "a379319c4473af14e1290bf6c7ddf68c",
+    "url": "/index.html"
+  }
+];
